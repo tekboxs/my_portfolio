831709b62222ec353a8d8551c4645d6fe7511cce
devandrewjpn <yuutoandrew.jpn@gmail.com>
2023-09-28 10:08:34 -0300

    useeffecet scroll handle to change menu active

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './nav.css'
 import {AiOutlineHome} from 'react-icons/ai'
 import {AiOutlineUser} from 'react-icons/ai'
@@ -6,9 +6,42 @@ import {BiBook} from 'react-icons/bi'
 import {RiServiceLine} from 'react-icons/ri'
 import {BiMessageSquareDetail} from 'react-icons/bi'
 
+const scrollToNavMap = [
+  { from: 0, to: 554, nav: '#' },
+  { from: 555, to: 999, nav: '#about' },
+  { from: 1000, to: 1776, nav: '#experience' },
+  { from: 1777, to: 2332, nav: '#services' },
+  { from: 2333, to: 4999, nav: '#services' },
+  { from: 5000, to: 5499, nav: '#contact' },
+];
+
 const Nav = () => {
 
-  const [activeNav,setActiveNav] = useState('#')
+  const [scrollY, setScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    const newActiveNav = scrollToNavMap.find(
+      ({ from, to }) => scrollY >= from && scrollY <= to
+    );
+
+    if (newActiveNav) {
+      setActiveNav(newActiveNav.nav);
+    }
+  }, [scrollY]);
+
+  const [activeNav, setActiveNav] = useState('#');
 
   return (
     <nav>
